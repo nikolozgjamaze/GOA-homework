a24ca665558278205fe2af7f6584fd62f1041c54
nikolozjgamadze <nikolozjgamadze@gmail.com>
2025-04-11 19:31:13 +0400

    commit

---
+++ Level 26/Classwork/task.py
@@ -0,0 +1,13 @@
+
+surname = "ჯღამაზე"  
+
+first_letter = surname[0]
+
+
+last_letter = surname[-1]
+
+middle_letter = surname[len(surname) // 2]
+
+print("პირველი ასო:", first_letter)
+print("ბოლო ასო:", last_letter)
+print("შუა ასო:", middle_letter)
