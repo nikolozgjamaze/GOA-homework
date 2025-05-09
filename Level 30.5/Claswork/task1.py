f04d52adc7b8f6e728346e0001c1b7a0fc3c2e2e
nikolozjgamadze <nikolozjgamadze@gmail.com>
2025-05-09 20:01:25 +0400

    commit

---
+++ Level 30.5/Claswork/task1.py
@@ -0,0 +1,9 @@
+
+my_name = "nikolozi"
+user_name = input("Enter your name: ")
+if my_name.lower() == user_name.lower():
+    print("Our names are similar!")
+else:
+    print("We have different names.")
+
+
---
+++ Level 30.5/Homework/task.py
@@ -0,0 +1,11 @@
+
+my_word = "hello"
+
+
+user_word = input("Enter a word: ")
+
+
+if my_word.lower() == user_word.lower():
+    print("Our words are similar!")
+else:
+    print("We have different words.")
