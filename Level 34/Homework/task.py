c861ec38688df84a12fa4ebb3f86e8090add5aa3
nikolozjgamadze <nikolozjgamadze@gmail.com>
2025-06-06 12:43:44 +0400

    commit

---
+++ Level 34/Homework/task.py
@@ -0,0 +1,27 @@
+def stringis_gamravaleba(string, number):
+    return string * number
+
+stringis_gamravaleba("vano", 100)
+
+
+
+
+
+
+
+def str_len(string):
+    if len(string) > 10:
+        return string
+    else:
+        return "Too Short..."
+
+print(str_len("vano"))
+
+
+
+
+
+
+
+def number_to_string(num):
+    return str(num)
