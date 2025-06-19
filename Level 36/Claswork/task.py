673924d70d117764bb76e1d94f710806f745d7df
nikolozjgamadze <nikolozjgamadze@gmail.com>
2025-06-19 14:15:32 +0400

    commit

---
+++ Level 36/Claswork/task.py
@@ -0,0 +1,62 @@
+def check_surname(surname):
+   
+    if surname.lower().startswith('g'):
+        return True
+    else:
+        return False
+    
+    
+    
+    
+    
+    
+
+def reverse_sign(number):
+    return -abs(number)
+
+
+
+
+
+
+
+def round_fraction(fraction, digits):
+    return round(fraction, digits)
+
+
+
+
+
+def sort_string(s):
+    return ''.join(sorted(s))
+
+
+
+
+
+def join_list_with_symbol(lst, symbol):
+    return symbol.join(lst)
+
+
+
+
+
+sentence = input("OKOLOZI ")
+words = sentence.split()
+print(words)
+
+
+
+
+
+
+
+
+
+
+
+sentence = input("NIKOLOZI ")
+words = sentence.split()
+print(words)
+
+
