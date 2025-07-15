c716a24b85810fc49f84bc660432dad1ef7e01dd
nikolozjgamadze <nikolozjgamadze@gmail.com>
2025-07-15 20:23:16 +0400

    commit

---
+++ Level 38/Homework/task.py
@@ -0,0 +1,90 @@
+def find_smallest_int(arr):
+    return min(arr)
+
+
+
+
+
+
+
+
+
+
+
+def greet():
+    return "hello world!"
+
+
+
+
+
+
+
+
+
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+
+
+
+
+
+
+
+
+
+
+
+def no_space(x):
+    return x.replace(" ", "")
+
+
+
+
+
+
+
+
+
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+def add_binary(a, b):
+    return bin(a + b)[2:]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def sum_array(a):
+    return sum(a)
---
+++ Level 40/Classwork/task.py
@@ -0,0 +1,18 @@
+def find_smallest_int(arr):
+    return min(arr)
+
+
+
+def greet():
+    return "hello world!"
+
+
+
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+
+
+def no_space(x):
+    return x.replace(" ", "")
